f5a8e5111324fbfb1a3fc2f9f4dcb977dd638f55
Renzo Garcia <jore_ma@yahoo.com.ar>
2021-12-15 16:13:39 -0300

    adding video pop-up

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,56 @@
 // scroll to top functionality
-const scrollUp = document.querySelector("#scroll-up");
+const scrollUp = document.querySelector('#scroll-up');
 
-scrollUp.addEventListener("click", () => {
+scrollUp.addEventListener('click', () => {
   window.scrollTo({
     top: 0,
     left: 0,
-    behavior: "smooth",
+    behavior: 'smooth',
   });
 });
 
 // Nav hamburgerburger selections
 
-const burger = document.querySelector("#burger-menu");
-const ul = document.querySelector("nav ul");
-const nav = document.querySelector("nav");
+const burger = document.querySelector('#burger-menu');
+const ul = document.querySelector('nav ul');
+const nav = document.querySelector('nav');
 
-burger.addEventListener("click", () => {
-  ul.classList.toggle("show");
+burger.addEventListener('click', () => {
+  ul.classList.toggle('show');
 });
 
 // Close hamburger menu when a link is clicked
 
 // Select nav links
-const navLink = document.querySelectorAll(".nav-link");
+const navLink = document.querySelectorAll('.nav-link');
 
 navLink.forEach((link) =>
-  link.addEventListener("click", () => {
-    ul.classList.remove("show");
+  link.addEventListener('click', () => {
+    ul.classList.remove('show');
   })
 );
+
+// video frame 3
+window.document.onkeydown = function (e) {
+  if (!e) {
+    e = event;
+  }
+  if (e.keyCode == 27) {
+    close_video();
+  }
+};
+
+function open_video() {
+  var lightBoxVideo = document.getElementById('iframeVideo');
+  document.getElementById('headerPopup').style.display = 'inline';
+  lightBoxVideo.src = 'https://www.youtube.com/embed/0JBvuEEQ4yM';
+
+  document.getElementById('fade').style.display = 'block';
+}
+
+function close_video() {
+  var lightBoxVideo = document.getElementById('iframeVideo');
+  document.getElementById('headerPopup').style.display = 'none';
+  lightBoxVideo.src = '';
+  document.getElementById('fade').style.display = 'none';
+}
